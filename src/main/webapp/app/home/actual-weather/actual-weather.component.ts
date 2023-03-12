988640b39d7e4288391f2ad3aecb364081512c20
Marek Fryger <marekfryger@o2.pl>
2023-03-12 14:36:07 +0100

    start add actual weather componet

---
+++ src/main/webapp/app/home/actual-weather/actual-weather.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-actual-weather',
+  templateUrl: './actual-weather.component.html',
+  styleUrls: ['./actual-weather.component.scss'],
+})
+export class ActualWeatherComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { ActualWeatherComponent } from './actual-weather/actual-weather.component';
 
 @NgModule({
   imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ActualWeatherComponent],
 })
 export class HomeModule {}
